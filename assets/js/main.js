0ab192c5dbe4dfd589e4addf41b1bda55208d45d
Artem Demeniev <myfirstmillio93@gmail.com>
2025-01-26 15:47:00 +0200

    Updates responsive styles for toggle navigation and swiper breakpoints
    Adjusts padding and font size for toggle navigation on smaller screens
    Updates swiper breakpoints for better slide display on different screen sizes
    
    Updates responsive styles for toggle navigation and swiper breakpoints
    
    Adjusts padding and font size for toggle navigation on smaller screens
    Updates swiper breakpoints for better slide display on different screen sizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@ var swiper = new Swiper(".swiper", {
     spaceBetween: 30,
     centeredSlides: false,
     slidesPerView: 4,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true
@@ -16,18 +16,15 @@ var swiper = new Swiper(".swiper", {
         prevEl: ".swiper-button-prev"
     },
     breakpoints: {
-        // экраны >= 480px
         480: {
             slidesPerView: 1,
             spaceBetween: 20,
             centeredSlides: true,
         },
-        // экраны >= 768px
         768: {
             slidesPerView: 3,
             spaceBetween: 30,
         },
-        // экраны >= 1024px
         1024: {
             slidesPerView: 4,
             spaceBetween: 30,
